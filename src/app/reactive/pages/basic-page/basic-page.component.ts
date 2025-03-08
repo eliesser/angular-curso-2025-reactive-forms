c07e6ad6174e92f7820c39a151f2007724d66157
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-08 13:32:52 +0100

    refactor basic page component; update form labels and placeholders to English, implement reactive form structure

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,22 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  myForm = new FormGroup({
+    name: new FormControl('', [], []),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });
+}
