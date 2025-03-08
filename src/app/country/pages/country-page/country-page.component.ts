24c10fc1e33080a1bef2d522dce96a7aa905bdb4
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-08 15:03:21 +0100

    refactor(country-page): remove unnecessary line for cleaner code

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -67,7 +67,6 @@ export class CountryPageComponent {
           this.countryService.getCountryNamesByCodeArray(country.borders)
         )
       )
-
       .subscribe((borders) => {
         this.borders.set(borders);
       });
