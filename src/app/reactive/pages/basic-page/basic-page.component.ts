14e5a02af547fead99abda5f5bc0168b83fbb393
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-08 13:42:59 +0100

    implement form validation utilities; enhance error handling in basic page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -7,6 +7,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -14,9 +15,12 @@ import {
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  myForm = new FormGroup({
-    name: new FormControl('', [], []),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 }
